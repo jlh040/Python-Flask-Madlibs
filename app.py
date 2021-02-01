0e2597f4bc4e16fbc40024d033a865abc3e913de
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-01 02:53:32 -0700

    added html and a css file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from stories import Story, story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'x74kj5'
@@ -8,4 +9,8 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/home')
 def show_form():
-    return render_template('home.html')
+    ans = {'place': 'london', 'noun': 'Bird', 'verb': 'jump', 'adjective': 'funny',
+     'plural_noun': 'cars'}
+    our_prompts = story.prompts
+
+    return render_template('home.html', our_prompts = our_prompts)
